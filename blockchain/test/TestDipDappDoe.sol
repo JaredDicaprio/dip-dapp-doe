3a1c74d59e2b8afb65475ff179bc791d5fe9c371
Jordi Moraleda <jordi@tvrbo.pro>
2018-08-14 23:59:11 +0200

    Game acceptance specs

--- blockchain/test/TestDipDappDoe.sol
+++ blockchain/test/TestDipDappDoe.sol
@@ -7,24 +7,24 @@ import "../contracts/DipDappDoe.sol";
 import "../contracts/LibString.sol";
 
 contract TestDipDappDoe {
-    DipDappDoe games;
+    DipDappDoe gamesInstance;
 
     constructor() public {
-        games = DipDappDoe(DeployedAddresses.DipDappDoe());
+        gamesInstance = DipDappDoe(DeployedAddresses.DipDappDoe());
     }
 
     function testInitiallyEmpty() public {
-        Assert.equal(games.getOpenGames().length, 0, "The games array should be empty at the begining");
+        Assert.equal(gamesInstance.getOpenGames().length, 0, "The games array should be empty at the begining");
     }
 
     function testHashingFunction() public {
-        string memory hash1 = games.saltedHash(123, "my salt goes here");
+        string memory hash1 = gamesInstance.saltedHash(123, "my salt goes here");
         string memory hashA = LibString.saltedHash(123, "my salt goes here");
         
-        string memory hash2 = games.saltedHash(123, "my salt goes 2 here");
+        string memory hash2 = gamesInstance.saltedHash(123, "my salt goes 2 here");
         string memory hashB = LibString.saltedHash(123, "my salt goes 2 here");
         
-        string memory hash3 = games.saltedHash(234, "my salt goes here");
+        string memory hash3 = gamesInstance.saltedHash(234, "my salt goes here");
         string memory hashC = LibString.saltedHash(234, "my salt goes here");
         
         Assert.isNotEmpty(hash1, "Salted hash should be a valid string");
@@ -48,15 +48,15 @@ contract TestDipDappDoe {
         uint lastTransaction1;
         uint lastTransaction2;
 
-        string memory hash = games.saltedHash(123, "my salt goes here");
-        uint32 gameIdx = games.createGame(hash, "John");
+        string memory hash = gamesInstance.saltedHash(123, "my salt goes here");
+        uint32 gameIdx = gamesInstance.createGame(hash, "John");
         Assert.equal(uint(gameIdx), 0, "The first game should have index 0");
 
-        uint32[] memory openGames = games.getOpenGames();
+        uint32[] memory openGames = gamesInstance.getOpenGames();
         Assert.equal(openGames.length, 1, "One game should have been created");
         Assert.equal(uint(openGames[0]), 0, "The first game should have index 0");
 
-        (cells, status, amount, nick1, nick2) = games.getGameInfo(gameIdx);
+        (cells, status, amount, nick1, nick2) = gamesInstance.getGameInfo(gameIdx);
         Assert.equal(uint(cells[0]), 0, "The board should be empty");
         Assert.equal(uint(cells[1]), 0, "The board should be empty");
         Assert.equal(uint(cells[2]), 0, "The board should be empty");
@@ -71,9 +71,50 @@ contract TestDipDappDoe {
         Assert.equal(nick1, "John", "The nick should be John");
         Assert.isEmpty(nick2, "Nick2 should be empty");
 
-        (created, lastTransaction1, lastTransaction2) = games.getGameTimestamps(gameIdx);
+        (created, lastTransaction1, lastTransaction2) = gamesInstance.getGameTimestamps(gameIdx);
         Assert.isAbove(created, 0, "Creation date should be set");
         Assert.isAbove(lastTransaction1, 0, "The first player's transaction timestamp should be set");
         Assert.equal(lastTransaction2, 0, "The second player's transaction timestamp should be empty");
+    }
+
+    function testGameAccepted() public {
+        uint8[9] memory cells;
+        uint8 status;
+        uint amount;
+        string memory nick1;
+        string memory nick2;
+        uint created;
+        uint lastTransaction1;
+        uint lastTransaction2;
+
+        uint32[] memory openGames = gamesInstance.getOpenGames();
+        Assert.equal(openGames.length, 1, "One game should be available");
+
+        gamesInstance.acceptGame(openGames[0], 0, "Mary");
+
+        openGames = gamesInstance.getOpenGames();
+        Assert.equal(openGames.length, 1, "One game should still exist");
+        Assert.equal(uint(openGames[0]), 0, "The game should still have index 0");
+
+        (cells, status, amount, nick1, nick2) = gamesInstance.getGameInfo(openGames[0]);
+        Assert.equal(uint(cells[0]), 0, "The board should be empty");
+        Assert.equal(uint(cells[1]), 0, "The board should be empty");
+        Assert.equal(uint(cells[2]), 0, "The board should be empty");
+        Assert.equal(uint(cells[3]), 0, "The board should be empty");
+        Assert.equal(uint(cells[4]), 0, "The board should be empty");
+        Assert.equal(uint(cells[5]), 0, "The board should be empty");
+        Assert.equal(uint(cells[6]), 0, "The board should be empty");
+        Assert.equal(uint(cells[7]), 0, "The board should be empty");
+        Assert.equal(uint(cells[8]), 0, "The board should be empty");
+        Assert.equal(uint(status), 0, "The game should not be started");
+        Assert.equal(amount, 0, "The initial amount should be zero");
+        Assert.equal(nick1, "John", "The nick should be John");
+        Assert.equal(nick2, "Mary", "The nick should be Mary");
+
+        (created, lastTransaction1, lastTransaction2) = gamesInstance.getGameTimestamps(openGames[0]);
+        Assert.isAbove(created, 0, "Creation date should be set");
+        Assert.isAbove(lastTransaction1, 0, "The first player's transaction timestamp should be set");
+        Assert.isAbove(lastTransaction2, 0, "The second player's transaction timestamp should be set");
+
     }
 }
--- blockchain/test/dipDappDoe.js
+++ blockchain/test/dipDappDoe.js
@@ -39,15 +39,22 @@ contract('DipDappDoe', function (accounts) {
     });
 
     it("should create a game with no money", async function () {
+        var eventWatcher = gamesInstance.GameCreated();
         let hash = await libStringInstance.saltedHash.call(123, "my salt 1");
 
         await gamesInstance.createGame(hash, "John");
 
         assert.equal(await web3.eth.getBalance(gamesInstance.address).toNumber(), 0, "The contract should have registered a zero amount of ether owed to the players");
-        
+
         let gamesIdx = await gamesInstance.getOpenGames.call();
         gamesIdx = gamesIdx.map(n => n.toNumber());
         assert.deepEqual(gamesIdx, [0], "Should have one game");
+
+        const emittedEvents = await eventWatcher.get();
+        assert.isOk(emittedEvents, "Events should be an array");
+        assert.equal(emittedEvents.length, 1, "There should be one event");
+        assert.isOk(emittedEvents[0], "There should be one event");
+        assert.equal(emittedEvents[0].args.gameIdx.toNumber(), 0, "The game should have index zero");
 
         const gameIdx = gamesIdx[0];
 
@@ -73,16 +80,23 @@ contract('DipDappDoe', function (accounts) {
     });
 
     it("should create a game with money", async function () {
+        var eventWatcher = gamesInstance.GameCreated();
         let hash = await libStringInstance.saltedHash.call(123, "my salt 1");
 
         await gamesInstance.createGame(hash, "Jane", { value: web3.toWei(0.01, 'ether') });
 
         let balance = await web3.eth.getBalance(gamesInstance.address);
         assert.equal(balance.comparedTo(web3.toWei(0.01, 'ether')), 0, "The contract should have registered 0.01 ether owed to the players");
-        
+
         let gamesIdx = await gamesInstance.getOpenGames.call();
         gamesIdx = gamesIdx.map(n => n.toNumber());
         assert.deepEqual(gamesIdx, [0, 1], "Should have two games");
+
+        const emittedEvents = await eventWatcher.get();
+        assert.isOk(emittedEvents, "Events should be an array");
+        assert.equal(emittedEvents.length, 1, "There should be one event");
+        assert.isOk(emittedEvents[0], "There should be one event");
+        assert.equal(emittedEvents[0].args.gameIdx.toNumber(), 1, "The game should have index zero");
 
         const gameIdx = gamesIdx[1];
 
@@ -90,7 +104,7 @@ contract('DipDappDoe', function (accounts) {
         cells = cells.map(n => n.toNumber());
         assert.deepEqual(cells, [0, 0, 0, 0, 0, 0, 0, 0, 0], "The board should be empty");
         assert.equal(status.toNumber(), 0, "The game should not be started");
-        
+
         assert.equal(amount.comparedTo(web3.toWei(0.01, 'ether')), 0, "The game should have 0.01 ether");
         assert.equal(nick1, "Jane", "The player 1 should be Jane");
         assert.equal(nick2, "", "The player 2 should be empty");
@@ -106,5 +120,154 @@ contract('DipDappDoe', function (accounts) {
         assert.equal(player1, accounts[0], "The address of player 1 should be set");
         assert.equal(player2, "0x0000000000000000000000000000000000000000", "The address of player 2 should be empty");
         assert.deepEqual(rest3, [], "The response should have 2 elements");
+    });
+
+    it("should reject accepting a non-existing game", async function () {
+        try {
+            await gamesInstance.acceptGame(1234, 0, "Mary");
+            assert.fail("The transaction should have thrown an error");
+        }
+        catch (err) {
+            assert.include(err.message, "revert", "The transaction should be reverted");
+        }
+    });
+
+    it("should reject accepting games with a different amount of money than expected", async function () {
+        let hash = await libStringInstance.saltedHash.call(123, "my salt 1");
+        await gamesInstance.createGame(hash, "Johny", { value: web3.toWei(0.02, 'ether') });
+
+        let balance = await web3.eth.getBalance(gamesInstance.address);
+        assert.equal(balance.comparedTo(web3.toWei(0.03, 'ether')), 0, "The contract should have registered 0.02 ether owed to the players");
+
+        let gamesIdx = await gamesInstance.getOpenGames.call();
+        gamesIdx = gamesIdx.map(n => n.toNumber());
+        assert.deepEqual(gamesIdx, [0, 1, 2], "Should have two games");
+
+        const gameIdx = gamesIdx[2];
+
+        let [cells, status, amount, nick1, nick2, ...rest] = await gamesInstance.getGameInfo(gameIdx);
+        cells = cells.map(n => n.toNumber());
+        assert.deepEqual(cells, [0, 0, 0, 0, 0, 0, 0, 0, 0], "The board should be empty");
+        assert.equal(status.toNumber(), 0, "The game should not be started");
+
+        assert.equal(amount.comparedTo(web3.toWei(0.02, 'ether')), 0, "The game should have 0.02 ether");
+        assert.equal(nick1, "Johny", "The player 1 should be Johny");
+        assert.equal(nick2, "", "The player 2 should be empty");
+        assert.deepEqual(rest, [], "The response should have 5 elements");
+
+        try {
+            await gamesInstance.acceptGame(gameIdx, 0, "Kathy");
+            assert.fail("The transaction should have thrown an error");
+        }
+        catch (err) {
+            assert.include(err.message, "revert", "The transaction should be reverted");
+        }
+    });
+
+    it("should accept an available game", async function () {
+        var eventWatcher = gamesInstance.GameAccepted();
+        let hash = await libStringInstance.saltedHash.call(123, "my salt 1");
+
+        // create game
+        await gamesInstance.createGame(hash, "James", { value: web3.toWei(0.005, 'ether') });
+
+        let balance = await web3.eth.getBalance(gamesInstance.address);
+        assert.equal(balance.comparedTo(web3.toWei(0.035, 'ether')), 0, "The contract should have registered 0.005 ether owed to the players");
+
+        let gamesIdx = await gamesInstance.getOpenGames.call();
+        gamesIdx = gamesIdx.map(n => n.toNumber());
+        assert.deepEqual(gamesIdx, [0, 1, 2, 3], "Should have three games");
+
+        const gameIdx = gamesIdx[gamesIdx.length - 1];
+
+        let [cells, status, amount, nick1, nick2, ...rest] = await gamesInstance.getGameInfo(gameIdx);
+        cells = cells.map(n => n.toNumber());
+        assert.deepEqual(cells, [0, 0, 0, 0, 0, 0, 0, 0, 0], "The board should be empty");
+        assert.equal(status.toNumber(), 0, "The game should not be started");
+
+        assert.equal(amount.comparedTo(web3.toWei(0.005, 'ether')), 0, "The game should have 0.005 ether");
+        assert.equal(nick1, "James", "The player 1 should be James");
+        assert.equal(nick2, "", "The player 2 should be still empty");
+        assert.deepEqual(rest, [], "The response should have 5 elements");
+
+        // accept game
+        await gamesInstance.acceptGame(gameIdx, 0, "Kathy", { value: web3.toWei(0.005, 'ether'), from: accounts[1] });
+
+        balance = await web3.eth.getBalance(gamesInstance.address);
+        assert.equal(balance.comparedTo(web3.toWei(0.04, 'ether')), 0, "The contract should have registered 0.005 more ether owed to the players");
+
+        gamesIdx = await gamesInstance.getOpenGames.call();
+        gamesIdx = gamesIdx.map(n => n.toNumber());
+        assert.deepEqual(gamesIdx, [0, 1, 2, 3], "Should have three games");
+
+        const emittedEvents = await eventWatcher.get();
+        assert.isOk(emittedEvents, "Events should be an array");
+        assert.equal(emittedEvents.length, 1, "There should be one accepted game event");
+        assert.isOk(emittedEvents[0], "There should be one accepted game event");
+        assert.equal(emittedEvents[0].args.gameIdx.toNumber(), gameIdx, "The game should have the last gameIdx");
+
+        [cells, status, amount, nick1, nick2, ...rest] = await gamesInstance.getGameInfo(gameIdx);
+        cells = cells.map(n => n.toNumber());
+        assert.deepEqual(cells, [0, 0, 0, 0, 0, 0, 0, 0, 0], "The board should be empty");
+        assert.equal(status.toNumber(), 0, "The game should not be started yet");
+
+        assert.equal(amount.comparedTo(web3.toWei(0.01, 'ether')), 0, "The game should have 0.01 ether");
+        assert.equal(nick1, "James", "The player 1 should be James");
+        assert.equal(nick2, "Kathy", "The player 2 should be Kathy");
+        assert.deepEqual(rest, [], "The response should have 5 elements");
+
+        [created, lastTransaction1, lastTransaction2, ...rest2] = await gamesInstance.getGameTimestamps(gameIdx);
+        assert.isAbove(created.toNumber(), 0, "The creation timestamp should be set");
+        assert.isAbove(lastTransaction1.toNumber(), 0, "The last timestamp of player 1 should be set");
+        assert.isAbove(lastTransaction2.toNumber(), 0, "The last timestamp of player 2 should be set");
+        assert.deepEqual(rest2, [], "The response should have 3 elements");
+
+        [player1, player2, ...rest3] = await gamesInstance.getGamePlayers(gameIdx);
+        assert.equal(player1, accounts[0], "The address of player 1 should be set");
+        assert.equal(player2, accounts[0], "The address of player 2 should be empty");
+        assert.deepEqual(rest3, [], "The response should have 2 elements");
+    });
+
+    it("should reject accepting an already started game", async function () {
+        var eventWatcher = gamesInstance.GameAccepted();
+        let hash = await libStringInstance.saltedHash.call(123, "my salt 1");
+        await gamesInstance.createGame(hash, "Jim");
+
+        gamesIdx = await gamesInstance.getOpenGames.call();
+        gamesIdx = gamesIdx.map(n => n.toNumber());
+        assert.deepEqual(gamesIdx, [0, 1, 2, 3, 4], "Should have four games");
+
+        const gameIdx = gamesIdx[gamesIdx.length - 1];
+        await gamesInstance.acceptGame(gameIdx, 0, "Dana", {from: accounts[1]});
+
+        const emittedEvents = await eventWatcher.get();
+        assert.isOk(emittedEvents, "Events should be an array");
+        assert.equal(emittedEvents.length, 1, "There should be one accepted game event");
+        assert.isOk(emittedEvents[0], "There should be one accepted game event");
+        assert.equal(emittedEvents[0].args.gameIdx.toNumber(), gameIdx, "The game should have the last gameIdx");
+
+        try {
+            await gamesInstance.acceptGame(gameIdx, 0, "Dana", {from: accounts[2]});
+            assert.fail("The transaction should have thrown an error");
+        }
+        catch (err) {
+            assert.include(err.message, "revert", "The transaction should be reverted");
+        }
+        
+        try {
+            await gamesInstance.acceptGame(gameIdx, 1, "Donna", {from: accounts[1]});
+            assert.fail("The transaction should have thrown an error");
+        }
+        catch (err) {
+            assert.include(err.message, "revert", "The transaction should be reverted");
+        }
+        
+        try {
+            await gamesInstance.acceptGame(gameIdx, 1, "Dolly");
+            assert.fail("The transaction should have thrown an error");
+        }
+        catch (err) {
+            assert.include(err.message, "revert", "The transaction should be reverted");
+        }
     });
 });
